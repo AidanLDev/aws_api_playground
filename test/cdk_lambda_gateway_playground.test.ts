09d32d10b703c768abbd7fc3f7198693d74825ae
Aidan Lowson <dev@aidanlowson.com>
2025-05-19 20:27:43 +0100

    Initial commit

---
+++ test/cdk_lambda_gateway_playground.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaGatewayPlayground from '../lib/cdk_lambda_gateway_playground-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_lambda_gateway_playground-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaGatewayPlayground.CdkLambdaGatewayPlaygroundStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
