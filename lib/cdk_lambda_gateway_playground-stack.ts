5e1a35ce795149b01fdc2dffc4d4387ea871c244
Aidan Lowson <dev@aidanlowson.com>
2025-05-20 19:06:25 +0100

    Parsing function from ts correctly

--- lib/cdk_lambda_gateway_playground-stack.ts
+++ lib/cdk_lambda_gateway_playground-stack.ts
@@ -1,3 +1,4 @@
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as cdk from "aws-cdk-lib";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import { Construct } from "constructs";
@@ -8,7 +9,7 @@ export class CdkLambdaGatewayPlaygroundStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const helloFunction = new lambda.Function(this, "HelloHandler", {
+    const helloFunction = new NodejsFunction(this, "HelloHandler", {
       runtime: lambda.Runtime.NODEJS_LATEST,
       handler: "hello.handler",
       code: lambda.Code.fromAsset(path.join(__dirname, "../lambda")),
